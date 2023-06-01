8bca90e9ed7596b834c4ab7c78a690b991f3ad84
guyofir21 <guy.ofir@wnwd.com>
2023-06-01 16:06:04 +0300

    Adding login page

--- server.js
+++ server.js
@@ -24,6 +24,43 @@ app.use(cors())
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+// Login page
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const sql = 'SELECT * FROM users WHERE username = ? AND password = ?';
+    const values = [username, password];
+
+    pool.query(sql, values, (err, results) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Error logging in' });
+        } else {
+            if (results.length > 0) {
+                res.json({ message: 'Login successful' });
+                res.redirect('/restaurants');
+            } else {
+                res.status(401).json({ error: 'Invalid credentials' });
+            }
+        }
+    });
+});
+
+// Registration page
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+    const sql = 'INSERT INTO users (username, password) VALUES (?, ?)';
+    const values = [username, password];
+
+    pool.query(sql, values, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Error registering user' });
+        } else {
+            res.json({ message: 'Registration successful', id: result.insertId });
+            res.redirect('/login');
+        }
+    });
+});
 app.get('/restaurants', (req, res) => {
     pool.query("SELECT * FROM fast_food_on_map.fast_food_restaurants where country = \"US\" LIMIT 100;", (err, results) => {
         if (err) {
